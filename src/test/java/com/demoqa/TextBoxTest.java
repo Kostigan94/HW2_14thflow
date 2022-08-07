8135e24ae3c154ea62ec1c370041a3dfe67a1236
Kostigan94 <kostigche@gmail.com>
2022-08-07 14:43:14 +0400

    Changed assertions and selectors

--- src/test/java/com/demoqa/TextBoxTest.java
+++ src/test/java/com/demoqa/TextBoxTest.java
@@ -28,32 +28,34 @@ public class TextBoxTest {
       $("#firstName").setValue("Kos");
       $("#lastName").setValue("Kosov");
       $("#userEmail").setValue("Kosov@mail.ru");
-      $("[for='gender-radio-1']").click();
+      $(byText("Male")).click();
+      //$("[for='gender-radio-1']").click();
       $("#userNumber").setValue("7777934256");
       $("#dateOfBirthInput").click();
       $(".react-datepicker__month-select").selectOption(5);
       $(".react-datepicker__year-select").selectOption("1992");
       $(".react-datepicker__day--018").click();
       $("#subjectsInput").setValue("Maths").pressEnter();
-      $("[for='hobbies-checkbox-1']").click();
-      $("#uploadPicture").uploadFile(new File("src/test/resources/fOwl.PNG"));
+      $(byText("Sports")).click();
+      //$("[for='hobbies-checkbox-1']").click();
+      $("#uploadPicture").uploadFromClasspath("fOwl.PNG");
+      //$("#uploadPicture").uploadFile(new File("src/test/resources/fOwl.PNG"));
       $("#currentAddress").setValue("Prospect Mira 123");
       $("#react-select-3-input").setValue("Rajasthan").pressEnter();
       $("#react-select-4-input").setValue("Jaipur").pressEnter();
       $("#submit").pressEnter();
 
       //Assertions
-        $(".modal-body").shouldHave(
-                text("Kos Kosov"),
-                text("Kosov@mail.ru"),
-                text("Male"),
-                text("7777934256"),
-                text("18 JUne,1992"),
-                text("Maths"),
-                text("Sports"),
-                text("fOwl.PNG"),
-                text("Prospect Mira 123"),
-                text("Rajasthan Jaipur"));
-                }
+        $(".modal-body").$(byText("Student Name")).sibling(0).shouldHave(text("Kos Kosov"));
+        $(".modal-body").$(byText("Student Email")).sibling(0).shouldHave(text("Kosov@mail.ru"));
+        $(".modal-body").$(byText("Gender")).sibling(0).shouldHave(text("Male"));
+        $(".modal-body").$(byText("Date of Birth")).sibling(0).shouldHave(text("18 JUne,1992"));
+        $(".modal-body").$(byText("Mobile")).sibling(0).shouldHave(text("7777934256"));
+        $(".modal-body").$(byText("Address")).sibling(0).shouldHave(text("Prospect Mira 123"));
+        $(".modal-body").$(byText("Subjects")).sibling(0).shouldHave(text("Maths"));
+        $(".modal-body").$(byText("Hobbies")).sibling(0).shouldHave(text("Sports"));
+        $(".modal-body").$(byText("Picture")).sibling(0).shouldHave(text("fOwl.PNG"));
+        $(".modal-body").$(byText("State and City")).sibling(0).shouldHave(text("Rajasthan Jaipur"));
+        }
     }
 
